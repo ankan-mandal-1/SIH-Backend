39abe7a22c95db65ef262448719ded66349392a4
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2024-09-08 20:53:19 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    const res = await mongoose.connect(process.env.MONGO_DB).then(() => {
+    await mongoose.connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
         console.log("DB Connected!")
     }).catch((error) => {
         console.log("Error in DB Connection: ", error)
